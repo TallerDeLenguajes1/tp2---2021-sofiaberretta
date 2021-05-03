96bbae3b70dec28550d03b05957811b37843ad14
Ana Sofia Berretta Gali <anasofiaberrettagali@gmail.com>
2021-05-03 15:46:13 -0300

    cambios

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -7,7 +7,7 @@ double mt[N][M];
 
 int main(){
     for(f = 0; f<N; f++) {
-        for(f = 0;f<N; f++) {
+        for(c = 0;c<N; c++) {
             printf("%lf ", mt[f][c]);
         }
     printf("\n");
